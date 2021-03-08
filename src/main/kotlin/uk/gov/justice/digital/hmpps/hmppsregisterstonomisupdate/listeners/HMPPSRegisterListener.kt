1793e58597575a68407c2b5ae18ab1c4a46c47b4
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2021-03-08 13:09:45 +0000

    DT-1536 Add basic queue handling (#4)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsregisterstonomisupdate/listeners/HMPPSRegisterListener.kt
@@ -0,0 +1,43 @@
+package uk.gov.justice.digital.hmpps.hmppsregisterstonomisupdate.listeners
+
+import com.google.gson.Gson
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.jms.annotation.JmsListener
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsregisterstonomisupdate.listeners.HMPPSRegisterListener.EventType.COURT_REGISTER_UPDATE
+import uk.gov.justice.digital.hmpps.hmppsregisterstonomisupdate.model.CourtUpdate
+import uk.gov.justice.digital.hmpps.hmppsregisterstonomisupdate.services.CourtRegisterUpdateService
+
+@Service
+class HMPPSRegisterListener(
+  private val courtRegisterUpdateService: CourtRegisterUpdateService,
+  private val gson: Gson
+) {
+
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  @JmsListener(destination = "\${sqs.queue.name}")
+  fun onRegisterChange(message: String) {
+    val sqsMessage: SQSMessage = gson.fromJson(message, SQSMessage::class.java)
+    log.info("Received message ${sqsMessage.MessageId}")
+    val changeEvent: RegisterChangeEvent = gson.fromJson(sqsMessage.Message, RegisterChangeEvent::class.java)
+    when (changeEvent.eventType) {
+      COURT_REGISTER_UPDATE -> courtRegisterUpdateService.updateCourtRegister(CourtUpdate(courtId = changeEvent.id))
+      else -> log.info("Received a message I wasn't expected $changeEvent")
+    }
+  }
+
+  data class RegisterChangeEvent(
+    val eventType: EventType,
+    val id: String
+  )
+
+  data class SQSMessage(val Message: String, val MessageId: String)
+
+  enum class EventType {
+    COURT_REGISTER_UPDATE, COURT_REGISTER_INSERT
+  }
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsregisterstonomisupdate/model/CourtUpdate.kt
@@ -0,0 +1,3 @@
+package uk.gov.justice.digital.hmpps.hmppsregisterstonomisupdate.model
+
+data class CourtUpdate(val courtId: String)
