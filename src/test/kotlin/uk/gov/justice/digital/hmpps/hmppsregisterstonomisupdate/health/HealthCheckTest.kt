4633c5d33f06c18a27e2744bbb35dbd1a16bffed
karenmillermoj <karen.miller@digital.justice.gov.uk>
2021-03-08 13:14:50 +0000

    DT-1536 Add Queue Health Check

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsregisterstonomisupdate/health/HealthCheckTest.kt
@@ -0,0 +1,79 @@
+package uk.gov.justice.digital.hmpps.hmppsregisterstonomisupdate.health
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.hmppsregisterstonomisupdate.integration.IntegrationTestBase
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.function.Consumer
+
+class HealthCheckTest : IntegrationTestBase() {
+
+  @Test
+  fun `Health page reports ok`() {
+    webTestClient.get()
+      .uri("/health")
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("status").isEqualTo("UP")
+  }
+
+  @Test
+  fun `Health info reports version`() {
+    webTestClient.get().uri("/health")
+      .exchange()
+      .expectStatus().isOk
+      .expectBody().jsonPath("components.healthInfo.details.version").value(
+        Consumer<String> {
+          assertThat(it).startsWith(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
+        }
+      )
+  }
+
+  @Test
+  fun `Queue health reports queue details`() {
+    webTestClient.get().uri("/health")
+      .exchange()
+      .expectStatus().isOk
+      .expectBody()
+      .jsonPath("components.hmppsDomainQueueHealth.details.MessagesOnQueue").isEqualTo(0)
+      .jsonPath("components.hmppsDomainQueueHealth.details.MessagesInFlight").isEqualTo(0)
+      .jsonPath("components.hmppsDomainQueueHealth.details.MessagesOnDLQ").isEqualTo(0)
+      .jsonPath("components.hmppsDomainQueueHealth.details.dlqStatus").isEqualTo("UP")
+  }
+
+  @Test
+  fun `Health ping page is accessible`() {
+    webTestClient.get()
+      .uri("/health/ping")
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("status").isEqualTo("UP")
+  }
+
+  @Test
+  fun `readiness reports ok`() {
+    webTestClient.get()
+      .uri("/health/readiness")
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("status").isEqualTo("UP")
+  }
+
+  @Test
+  fun `liveness reports ok`() {
+    webTestClient.get()
+      .uri("/health/liveness")
+      .exchange()
+      .expectStatus()
+      .isOk
+      .expectBody()
+      .jsonPath("status").isEqualTo("UP")
+  }
+}
