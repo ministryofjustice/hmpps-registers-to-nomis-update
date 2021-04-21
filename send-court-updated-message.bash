4156d9f11587276caff001fd7809ae35131905b7
petergphillips <peter@greenthistle.co.uk>
2021-04-21 15:59:44 +0100

    DT-1949: ⬆️ Update to latest localstack (#14)

--- send-court-updated-message.bash
+++ send-court-updated-message.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-aws --endpoint-url=http://localhost:4575 sns publish \
+aws --endpoint-url=http://localhost:4566 sns publish \
     --topic-arn arn:aws:sns:eu-west-2:000000000000:hmpps-domain-events \
     --message '{"eventType":"COURT_REGISTER_UPDATE","id":"SHFCC"}' \
     --message-attributes '{"eventType" : { "DataType":"String", "StringValue":"COURT_REGISTER_UPDATE"}}'
