02a24a9cf72333a385fbc535315697e233622de2
petergphillips <peter@greenthistle.co.uk>
2021-10-01 14:05:20 +0100

    DT-2662: ⬆️ Switch to java 17 (#54)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ configurations {
 }
 
 dependencies {
+  agentDeps("com.microsoft.azure:applicationinsights-agent:3.2.0-BETA.4")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("com.google.code.gson:gson:2.8.8")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.2")
