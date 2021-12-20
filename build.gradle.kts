0bbd29dec400e3b9dc058a8a80f34206d326a003
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2021-12-20 08:04:13 +0000

    DT-2970: ⬆️ Upgrade hmpps-gradle-springboot plugin to 4.0.0 (#70)
    * DT-2970: ⬆️ Upgrade hmpps-gradle-springboot plugin to 4.0.0
    includes springboot 2.6.1 and kotlin 1.6
    
    * DT-2970: add in InfoTest.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.16"
-  kotlin("plugin.spring") version "1.5.31"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.0.0"
+  kotlin("plugin.spring") version "1.6.0"
 }
 
 configurations {
@@ -9,20 +9,19 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("com.google.code.gson:gson:2.8.8")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.2")
+  implementation("com.google.code.gson:gson:2.8.9")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.0.3")
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
-  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.5.10")
+  implementation("org.springdoc:springdoc-openapi-webmvc-core:1.6.1")
 
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("org.awaitility:awaitility-kotlin:4.1.0")
+  testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.testcontainers:localstack:1.16.0")
-  testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+  testImplementation("org.testcontainers:localstack:1.16.2")
 }
 
 java {
