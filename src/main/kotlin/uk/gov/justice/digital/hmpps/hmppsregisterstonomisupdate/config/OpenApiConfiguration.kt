4465874b01be536cb8da3eaaf12eea13564ce868
Peter Phillips <peter@greenthistle.co.uk>
2022-02-28 10:42:24 +0000

    SDI-71: 📝 Copy date time fix and security improvements

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsregisterstonomisupdate/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsregisterstonomisupdate/config/OpenApiConfiguration.kt
@@ -1,8 +1,16 @@
 package uk.gov.justice.digital.hmpps.hmppsregisterstonomisupdate.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.media.DateTimeSchema
+import io.swagger.v3.oas.models.media.Schema
+import io.swagger.v3.oas.models.media.StringSchema
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
+import org.springdoc.core.customizers.OpenApiCustomiser
 import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -13,10 +21,50 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
 
   @Bean
   fun customOpenAPI(): OpenAPI = OpenAPI()
+    .servers(
+      listOf(
+        Server().url("https://registers-to-nomis-update-dev.hmpps.service.justice.gov.uk").description("Development"),
+        Server().url("https://registers-to-nomis-update-preprod.hmpps.service.justice.gov.uk").description("PreProd"),
+        Server().url("https://registers-to-nomis-update.hmpps.service.justice.gov.uk").description("Prod"),
+        Server().url("http://localhost:8080").description("Local"),
+      )
+    )
     .info(
       Info().title("HMPPS Registers to NOMIS sync")
         .version(version)
         .description("Syncing of UK Court Information to NOMIS")
         .contact(Contact().name("HMPPS Digital Studio").email("feedback@digital.justice.gov.uk"))
     )
+    .components(
+      Components().addSecuritySchemes(
+        "bearer-jwt",
+        SecurityScheme()
+          .type(SecurityScheme.Type.HTTP)
+          .scheme("bearer")
+          .bearerFormat("JWT")
+          .`in`(SecurityScheme.In.HEADER)
+          .name("Authorization")
+      )
+    )
+    .addSecurityItem(SecurityRequirement().addList("bearer-jwt", listOf("read", "write")))
+
+  @Bean
+  fun openAPICustomiser(): OpenApiCustomiser = OpenApiCustomiser {
+    it.components.schemas.forEach { (_, schema: Schema<*>) ->
+      val properties = schema.properties ?: mutableMapOf()
+      for (propertyName in properties.keys) {
+        val propertySchema = properties[propertyName]!!
+        if (propertySchema is DateTimeSchema) {
+          properties.replace(
+            propertyName,
+            StringSchema()
+              .example("2021-07-05T10:35:17")
+              .pattern("^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}$")
+              .description(propertySchema.description)
+              .required(propertySchema.required)
+          )
+        }
+      }
+    }
+  }
 }
