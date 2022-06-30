2f7947a4901412aaee8411cf5f2edced163f4f9a
petergphillips <peter@greenthistle.co.uk>
2022-06-30 13:35:15 +0100

    SDI-257: 🔒️ Suppress false positive for tomcat (#101)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.0-beta-1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.1"
   kotlin("plugin.spring") version "1.7.0"
 }
 
@@ -25,7 +25,7 @@ dependencies {
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("org.testcontainers:localstack:1.17.2")
+  testImplementation("org.testcontainers:localstack:1.17.3")
 }
 
 java {
